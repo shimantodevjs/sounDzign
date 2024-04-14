78cb2e3d2d1ca3d449bd122110be36e26ccefcd7
shimantodevjs <shimantodevjs@gmail.com>
2024-04-15 00:43:13 +0600

    final commit

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Header from './Header/Header'
-import Blog from './Blog/Blog'
-import Footer from './Footer/Footer'
-import Info from './Info/Info'
-import Feedback from './Feedback/Feedback'
-import Topics from './Topics/Topics'
-
-export {Header,Blog,Footer,Info,Feedback,Topics}
